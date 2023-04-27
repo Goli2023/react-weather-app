dae0d53451c296866438d8a987956e2989637a25
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-04-27 13:11:39 +0330

    Included the Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function () {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Goli and is open-sourced on
+        <a href="https://github.com/Goli2023/react-weather-app" target="-blank">
+          {" "}
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
