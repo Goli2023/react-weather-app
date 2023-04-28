a820119d8852c3e99c0e520205dda6af8da8f288
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-04-28 15:41:27 +0330

    Included Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import "./App.css";
 export default function () {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by Goli and is open-sourced on
-        <a href="https://github.com/Goli2023/react-weather-app" target="-blank">
-          {" "}
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by Goli and is open-sourced on
+          <a
+            href="https://github.com/Goli2023/react-weather-app"
+            target="-blank"
+          >
+            {" "}
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
