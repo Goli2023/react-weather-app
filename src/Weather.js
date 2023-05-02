a8707714d0b1dfbdcc020be8dd7c1d3f29a840af
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-05-02 14:12:39 +0330

    Added axios, apiKey, apiUrl, useState

--- src/Weather.js
+++ src/Weather.js
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function () {
-  return (
-    <div className="Weather">
-      <h1>Tehran</h1>
-      <p>Tuesday 10:06, Clear Sky</p>
-      <h2>
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt="mostly couldy"
-        />
-        <span className="temperature">17</span>
-        <span className="unit"> °C </span>
-      </h2>
-      <hr />
-      <div className="row">
-        <div className="col-6">Humidity:25%</div>
-        <div className="col-6">Wind:5 km/h</div>
-      </div>
-      <form className="mt-3">
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleSubmit(response) {
+    setTemperature(response.data.temperature.current);
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <h1>Tehran</h1>
+        <p>Tuesday 10:06, Clear Sky</p>
+        <h2>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly couldy"
+          />
+          <span className="temperature">{Math.round(temperature)}</span>
+          <span className="unit"> °C </span>
+        </h2>
+        <hr />
         <div className="row">
-          <div className="col-10">
-            <input
-              type="search"
-              placeholder="Type a city"
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-2">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
+          <div className="col-6">Humidity:25%</div>
+          <div className="col-6">Wind:5 km/h</div>
         </div>
-      </form>
-    </div>
-  );
+        <form className="mt-3">
+          <div className="row">
+            <div className="col-10">
+              <input
+                type="search"
+                placeholder="Type a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-2">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    const apiKey = "0td6fef782e8b046o3c9bf5a39fbbf05";
+    let city = "Tehran";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleSubmit);
+
+    return "Loading...";
+  }
 }
