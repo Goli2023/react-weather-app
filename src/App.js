071d3a9e2bc461990587313c57e72914c2419d00
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-05-02 14:40:45 +0330

    Added setDateWeather & replaced items based on that

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-export default function () {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
--- src/Weather.js
+++ src/Weather.js
@@ -2,30 +2,39 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function () {
+export default function Weather() {
   const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
   function handleSubmit(response) {
-    setTemperature(response.data.temperature.current);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+    });
+
     setReady(true);
   }
   if (ready) {
     return (
       <div className="Weather">
-        <h1>Tehran</h1>
-        <p>Tuesday 10:06, Clear Sky</p>
+        <h1>{weatherData.city}</h1>
+        <p>Tuesday 10:06, {weatherData.description}</p>
         <h2>
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="mostly couldy"
           />
-          <span className="temperature">{Math.round(temperature)}</span>
+          <span className="temperature">
+            {Math.round(weatherData.temperature)}
+          </span>
           <span className="unit"> °C </span>
         </h2>
         <hr />
         <div className="row">
-          <div className="col-6">Humidity:25%</div>
-          <div className="col-6">Wind:5 km/h</div>
+          <div className="col-6">Humidity:{weatherData.humidity} %</div>
+          <div className="col-6">Wind:{weatherData.wind} km/h</div>
         </div>
         <form className="mt-3">
           <div className="row">
@@ -47,7 +56,7 @@ export default function () {
   } else {
     const apiKey = "0td6fef782e8b046o3c9bf5a39fbbf05";
     let city = "Tehran";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${weatherData.city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleSubmit);
 
     return "Loading...";
