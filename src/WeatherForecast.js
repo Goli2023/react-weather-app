7ee92e4f12b398d74d183446529a827c775c45f5
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-05-09 17:08:50 +0330

    Tried to include image icon for the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
-export default function WeatherForcast() {
+export default function WeatherForcast(props) {
+  let [loaded, setLoaded] = useState(false);
   function handleResponse(response) {
-    console.log(response.data);
   }
 
-  let apiKey = "0td6fef782e8b046o3c9bf5a39fbbf05";
-  let city = "Tehran";
-  let icon =
-    "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  if (loaded) {
+    let apiKey = "0td6fef782e8b046o3c9bf5a39fbbf05";
+    let city = props.data.city;
+    let icon=props.data.daily.condition.icon;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <img src={icon} width={80} className="WeatherForecast-img" alt="" />
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">19°</span>
-            <span className="WeatherForecast-temp-min">10°</span>
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+            <img src={icon} width={80} className="WeatherForecast-img" alt="" />
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">19°</span>
+              <span className="WeatherForecast-temp-min">10°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
